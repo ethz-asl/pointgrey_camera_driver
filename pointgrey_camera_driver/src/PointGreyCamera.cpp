9e6e34fd08d466dbaf67c67141f5c3ecba7ce3c3
HannesSommer <hannes.sommer@mavt.ethz.ch>
2017-03-07 17:49:22 +0100

    Added switching convex hull algorithm (experimental)

--- pointgrey_camera_driver/src/PointGreyCamera.cpp
+++ pointgrey_camera_driver/src/PointGreyCamera.cpp
@@ -1009,9 +1009,13 @@ void PointGreyCamera::grabImage(sensor_msgs::Image &image, const std::string &fr
 
     double device_time = cumulative_timestamp_.seconds + cumulative_timestamp_.microSeconds*1.0/kSecondToUS;
 
-    timesync_.updateFilter(device_time, local_time);
-    double updated_local_time = timesync_.getLocalTimestamp(device_time);
-
+    double updated_local_time;
+    if(use_convexhull_timesync_){
+      updated_local_time = timestamp_corrector_.correctTimestamp(device_time, local_time, convex_hull_switching_time_);
+    } else {
+      timesync_.updateFilter(device_time, local_time);
+      updated_local_time = timesync_.getLocalTimestamp(device_time);
+    }
     image.header.stamp = ros::Time(updated_local_time);
 
     // Check the bits per pixel.
