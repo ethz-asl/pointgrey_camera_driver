0bbfbf3aae35a86685e2769ed2d48c0e21bef574
HannesSommer <hannes.sommer@mavt.ethz.ch>
2017-03-07 16:54:05 +0100

    Take local timestamp earlier

--- pointgrey_camera_driver/src/PointGreyCamera.cpp
+++ pointgrey_camera_driver/src/PointGreyCamera.cpp
@@ -983,6 +983,9 @@ void PointGreyCamera::grabImage(sensor_msgs::Image &image, const std::string &fr
     Image rawImage;
     // Retrieve an image
     Error error = cam_.RetrieveBuffer(&rawImage);
+    //get receive time in local clock
+    const double local_time = ros::Time::now().toSec();
+    
     PointGreyCamera::handleError("PointGreyCamera::grabImage Failed to retrieve buffer", error);
     metadata_ = rawImage.GetMetadata();
 
@@ -1004,9 +1007,7 @@ void PointGreyCamera::grabImage(sensor_msgs::Image &image, const std::string &fr
 
     last_timestamp_ = currentTime;
 
-    //get time in local clock
     double device_time = cumulative_timestamp_.seconds + cumulative_timestamp_.microSeconds*1.0/kSecondToUS;
-    double local_time = ros::Time::now().toSec();
 
     timesync_.updateFilter(device_time, local_time);
     double updated_local_time = timesync_.getLocalTimestamp(device_time);
