a499a81ea6b73bf02c921dfddbf9036b28204331
Zachary Taylor <zacharyjeremytaylor@gmail.com>
2017-08-15 17:36:26 +0200

    small changes to comments

--- pointgrey_camera_driver/src/debayered_camera_node.cpp
+++ pointgrey_camera_driver/src/debayered_camera_node.cpp
@@ -4,13 +4,13 @@
 int main(int argc, char** argv) {
   ros::init(argc, argv, "debayered_camera_node");
 
-  nodelet::Loader manager;  // Don't bring up the manager ROS API
+  nodelet::Loader manager;
   nodelet::V_string nargv;
   nodelet::M_string remap(ros::names::getRemappings());
   remap["image_raw"] = ros::names::resolve("~/image_raw");
 
   std::string nodelet_name = ros::this_node::getName();
-  
+
   manager.load(nodelet_name + "PointGreyCameraNodelet",
                "pointgrey_camera_driver/PointGreyCameraNodelet", remap, nargv);
   ROS_INFO_STREAM("Started " << nodelet_name << "/PointGreyCameraNodelet"
